05b08e1ca9bc5d35d4389f5a3e2e5b8912281015
Daniil Petrov <octrow@yandex.ru>
2023-03-05 20:55:42 +0600

    v3.1

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -6,7 +6,11 @@ from .models import Comment, Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ("text", "group", "image")
+        fields = (
+            "text",
+            "group",
+            # "image"
+        )
         labels = {
             "text": "Текст записи",
             "group": "Группа",
